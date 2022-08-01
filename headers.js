dae4743d61e0956b01fbea084def3d5a0feb6ddc
carsonfischl <carson.fischl@carleton.ca>
2022-08-01 13:02:01 -0400

    fixed medieval path

--- headers.js
+++ headers.js
@@ -19,6 +19,15 @@ export const headers = [
       },
       {
         id: '3',
+        title: 'Medieval',
+        category: 'medieval',
+        subcategory: 'medieval',
+        pic: '/images/pergamonheader.jpeg',
+        body:
+          'Hellenistic civilization is generally dated to the Death of Alexander III of Macedon in 323 BCE in Babylon. Following his death, the kingdoms left in the wake of his conquests continued to proselize Hellenistic values and culture acrooss the old world.',
+      },
+      {
+        id: '4',
         title: 'Early Modern',
         category: 'earlymodern',
         subcategory: 'earlymodern',
