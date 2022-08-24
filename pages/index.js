59ba4914418e575ae247546fcb89b3b3ca52c457
carsonfischl <carson.fischl@carleton.ca>
2022-08-24 15:18:37 -0400

    removed border radius on index page

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({items, main}) {
   return (
     <div className={homeStyles.container}>
       <h1 className={headerStyles.title}>Comitas Collection</h1>
-      <Image src={mainphoto} alt='mainphoto' style={{ width: '80%', borderRadius: '10%', padding: '1rem' }} />
+      <Image src={mainphoto} alt='mainphoto' style={{ width: '80%', padding: '1rem' }} />
       <h3 className={headerStyles.subtitle}>A Global Historiography through Numismatics</h3>
     </div>
   )
