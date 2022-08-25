52efc36c553214f587ce248afffa9cf255d5d9cd
carsonfischl <carson.fischl@carleton.ca>
2022-08-25 12:03:34 -0400

    added items to index

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,32 @@ import headerStyles from '../styles/Header.module.css'
 import Header from '../components/Header'
 import { server } from '../config'
 import ItemList from '../components/ItemList'
-import items from '../data.js'
+import items from '../data'
 import mainphoto from '../public/images/mainheader.jpeg'
 
-export default function Home({items, main}) {
+const Home = ({result, main}) => {
+  console.log(result)
   return (
     <div className={homeStyles.container}>
       <h1 className={headerStyles.title}>Comitas Collection</h1>
       <Image src={mainphoto} alt='mainphoto' style={{ width: '80%', padding: '1rem' }} />
       <h3 className={headerStyles.subtitle}>A Global Historiography through Numismatics</h3>
+      <div>
+        <ItemList items={result}/>
+      </div>
     </div>
   )
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/items`)
+  const result = await res.json()
+
+  return {
+    props: {
+      result,
+    },
+  }
+}
+
+export default Home
