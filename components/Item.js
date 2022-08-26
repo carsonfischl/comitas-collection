641e00e9cc1d263f29a11f5d0b316c4416747903
carsonfischl <carson.fischl@carleton.ca>
2022-08-26 14:30:50 -0400

    attempt to change link to black

--- components/Item.js
+++ components/Item.js
@@ -3,16 +3,17 @@ import { Card, Grid, Text, Loading } from "@nextui-org/react";
 import { useState } from 'react';
 import Loader from '../components/Loader'
 import spinner from '../public/images/spinner.svg'
+import styles from '../styles/Item.module.css'
 
 const Item = ({ item }) => {
   
   return (
     <Link href={`/item/${item.id}`}>
-      <Card flat isHoverable isPressable css={{ mw: "400px", alignContent: 'center'}}>
+      <Card flat isHoverable isPressable css={{ mw: "400px"}} className={styles.card}>
          <Card.Image src={item.pic} alt={item.id} layout='responsive' css={{ padding: '1rem', alignContent: 'center' }} placeholder='blur' blurDataURL={spinner}/>
-            <Card.Body>
-                <Text>{item.title}</Text>
-                <Text>{item.date}</Text>
+            <Card.Body className={styles.card}>
+                <Text className={styles.card}>{item.title}</Text>
+                <Text className={styles.card}>{item.date}</Text>
             </Card.Body>
         </Card>
     </Link>
--- pages/item/[id]/index.js
+++ pages/item/[id]/index.js
@@ -7,15 +7,16 @@ import { Card, Grid, Text, Loading } from "@nextui-org/react"
 import { items } from '../../../data'
 import { useState, useEffect } from 'react'
 import spinner from '../../../public/images/spinner.svg'
+import styles from '../../../styles/Item.module.css'
 
 const item = ({ result }) => {
 
   return (
     <>
       <Meta title={result.title} description={result.excerpt} />
-      <Card style={{ alignContent: 'center', justifyContent: 'center'}}>
+      <Card className={styles.card}>
         <Card.Image src={result.pic} alt={result.id} layout='responsive' css={{ padding: '1rem', alignContent: 'center' }} placeholder='blur' blurDataURL={spinner}/>
-        <Card.Body>
+        <Card.Body className={styles.card}>
           <Card.Header>{result.title}</Card.Header>
             {result.body}
             <strong>Date: </strong>{result.date}<br/>
@@ -25,7 +26,7 @@ const item = ({ result }) => {
         </Card.Body>
       </Card>
       <br />
-      <Link href='/' css={{color: '$black', paddingBottom: '1rem' }}>Go Back</Link>
+      <Link href='/' css={{paddingBottom: '1rem' }} className={styles.link}>Go Back</Link>
     </>
   )
 }
