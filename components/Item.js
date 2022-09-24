84935d1798fab467f3ef653e1b5d41c8c23c16a4
carsonfischl <carson.fischl@carleton.ca>
2022-09-24 14:45:13 -0400

    third spinner attempt

--- components/Item.js
+++ components/Item.js
@@ -1,17 +1,30 @@
 import Link from 'next/link'
-import { Card, Grid, Text, Loading } from "@nextui-org/react";
-import { useState } from 'react';
+import { Card, Grid, Text, Loading, StyledPre } from "@nextui-org/react";
+import { useState, useEffect } from 'react';
 import Loader from '../components/Loader'
 import spinner from '../public/images/spinner.svg'
 
 const Item = ({ item }) => {
 
   const [isLoading, setIsLoading] = useState(false);
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const result = await {item};   
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
   
   return (
     <Link href={`/item/${item.id}`}>
       <Card flat isHoverable isPressable css={{ mw: "400px", alignContent: 'center'}}>
+        {isLoading ? (
+          <Loader />
+        ) : (
          <Card.Image src={item.pic} alt={item.id} layout='responsive' css={{ alignContent: 'center'}} placeholder='blur' blurDataURL='/../public/images/spinner.svg'/>
+        )}
             <Card.Body css={{ display: 'contents', textAlign: 'center', display: 'inline-grid'}}>
                 <Text>{item.title}</Text>
                 <Text>{item.date}</Text>
