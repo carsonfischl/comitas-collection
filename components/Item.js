d82fdbd42f5bc8d1872780d97ee299ead47231bf
carsonfischl <carson.fischl@carleton.ca>
2022-09-24 14:16:52 -0400

    removed image padding ugh

--- components/Item.js
+++ components/Item.js
@@ -11,7 +11,7 @@ const Item = ({ item }) => {
   return (
     <Link href={`/item/${item.id}`}>
       <Card flat isHoverable isPressable css={{ mw: "400px", alignContent: 'center'}}>
-         <Card.Image src={item.pic} alt={item.id} layout='responsive' css={{ alignContent: 'center', padding: '1rem'}} placeholder='blur' blurDataURL='/../public/images/spinner.svg'/>
+         <Card.Image src={item.pic} alt={item.id} layout='responsive' css={{ alignContent: 'center'}} placeholder='blur' blurDataURL='/../public/images/spinner.svg'/>
             <Card.Body css={{ display: 'contents', textAlign: 'center'}}>
                 <Text>{item.title}</Text>
                 <Text>{item.date}</Text>
