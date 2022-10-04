dc2d9379a307d5dbfb644da56071a2b7063d6415
carsonfischl <carson.fischl@carleton.ca>
2022-10-04 11:54:42 -0400

    got icons

--- components/Nav.js
+++ components/Nav.js
@@ -3,6 +3,12 @@ import navStyles from "../styles/Nav.module.css";
 import React from "react";
 import Form from "./Form";
 
+import { GiRomanShield } from 'react-icons/gi';
+import { GiGreekSphinx } from 'react-icons/gi';
+import { GiMedievalPavilion } from 'react-icons/gi';
+import { GiTowerBridge } from 'react-icons/gi';
+
+
 export default function App() {
     const [visible, setVisible] = React.useState(false);
     const handler = () => setVisible(true);
