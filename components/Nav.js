14df4359f95902a47967ee308eb4166071c08cb0
carsonfischl <carson.fischl@carleton.ca>
2022-10-06 11:44:18 -0400

    replaced class with className

--- components/Nav.js
+++ components/Nav.js
@@ -7,18 +7,18 @@ import { GiTowerBridge } from 'react-icons/gi';
 
 export default function App() {
   return (
-    <nav class="flex fixed w-screen h-11 bg-gray-500 border-b-4 z-10">
-      <div class="flex-1 flex justify-center mr-auto">
-        <a class="mx-2 p-2">
+    <nav className="flex fixed w-screen h-11 bg-gray-500 border-b-4 z-10">
+      <div className="flex-1 flex justify-center mr-auto">
+        <a className="mx-2 p-2">
           <GiRomanShield size="2em" />
         </a>
       </div>
-      <div class="mx-12">
+      <div className="mx-12">
         L
       </div>
-      <div class="flex-1 flex justify-center ml-auto">
-        <a class="mx-2 p-4">LINK THREEE</a>
-        <a class="mx-2 p-4">LINK FOOOUR</a>
+      <div className="flex-1 flex justify-center ml-auto">
+        <a className="mx-2 p-4">LINK THREEE</a>
+        <a className="mx-2 p-4">LINK FOOOUR</a>
     </div>
     </nav>
   );
