69b219e68acf40170d0e9c50f928888b4f6be3fd
carsonfischl <carson.fischl@carleton.ca>
2023-08-30 15:13:48 -0400

    added vercel analytics in Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,7 @@ import Nav from './Nav'
 import Header from './Header'
 import styles from '../styles/Layout.module.css'
 import Background from './Background'
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = ({ children }) => {
   return (
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
       <div className={styles.container}>
         <main className={styles.main}>
           {children}
+          <Analytics/>
         </main>
       </div>
     </>
